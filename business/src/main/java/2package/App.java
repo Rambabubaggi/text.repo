53e66ad4e7cd098400f70193c2f955f4c267b532
rambabu <ramlitam12@gmail.com>
2022-10-28 00:08:05 +0530

    new project

---
+++ business/src/main/java/2package/App.java
@@ -0,0 +1,13 @@
+package 2package;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
